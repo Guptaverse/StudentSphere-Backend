820c17c298e3ac1770e0b72f67b1fe66d3679891
SumitKrShakya <sumitshakya20101999@gmail.com>
2023-04-27 23:57:03 +0530

    update

--- db/mongo.js
+++ db/mongo.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URL);
+        await mongoose.connect(process.env.MONGODB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }).then(() => { console.warn("db connection done") });
     } catch (error) {
         throw error;
     }
